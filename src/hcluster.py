fe7e3f321468165c5d8fb1bf6ee67c8a029f627f
diorge <diorge.bs@gmail.com>
2016-06-08 20:50:24 -0300

    Distance matrix

--- src/hcluster.py
+++ src/hcluster.py
@@ -10,3 +10,18 @@ import math
 def euclidian(a, b):
     """ Euclidian distance in N-space """
     return math.sqrt(sum((x - y) ** 2 for (x, y) in zip(a, b)))
+
+
+def distance_matrix(data, dist):
+    """ Distance matrix (all-to-all) """
+    m = []
+    for elem1 in data:
+        m.append([])
+        for elem2 in data:
+            m[-1].append(dist(elem1, elem2))
+    return m
+
+
+def cluster(data, dist=None):
+    dist = dist or euclidian
+    m = distance_matrix(data, dist)
