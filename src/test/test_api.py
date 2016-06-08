20e2a39d611bbe2bfc672cb0268f7b2975dc1623
diorge <diorge.bs@gmail.com>
2016-06-08 20:42:26 -0300

    Basic API definition

---
+++ src/test/test_api.py
@@ -0,0 +1,31 @@
+"""
+API Tests for Hierarchial-Clustering interface
+These tests describe the required API of the module.
+"""
+
+
+import hcluster
+from unittest import TestCase
+
+
+class TestAPI(TestCase):
+    
+    def test_base_call(self):
+        """ Basic call - assumes Euclidian Distance """
+        data = [(5, 5), (5, 6), (1, 1), (0, 0)]
+        c = hcluster.cluster(data)
+        # expected structure:
+        #       |-------------|
+        #   |-------|         |
+        #   |       |     |-------|
+        # (0, 0) (1, 1) (5, 5) (5, 6)
+        expected = set(set((0, 0), (1, 1)), set((5, 5), (5, 6)))
+        assert expected == c.setrepr()
+
+    def test_cust_func(self):
+        """ Call passing a custom distance function """
+        data = [(5, 0), (0, 0), (1, 2)]
+        yDist = lambda a, b: abs(a[1] - b[1])
+        c = hcluster.cluster(data, yDist)
+        expected = set(set((5, 0), (0, 0)), (1, 2))
+        assert expected == c.setrepr()
