f2e1fd7d499d60b6a90574688c3a6230a40be93d
diorge <diorge.bs@gmail.com>
2016-06-08 20:58:01 -0300

    Distance matrix unit test

--- src/test/tests.py
+++ src/test/tests.py
@@ -4,6 +4,7 @@ Unit tests for the hcluster module
 
 
 import hcluster
+import math
 from unittest import TestCase
 
 
@@ -28,3 +29,10 @@ class TestHCluster(TestCase):
         c = hcluster.cluster(data, yDist)
         expected = set(set((5, 0), (0, 0)), (1, 2))
         assert expected == c.setrepr()
+
+    def test_distance_matrix(self):
+        data = [(5, 5), (5, 6), (4, 4)]
+        m = hcluster.distance_matrix(data, hcluster.euclidian)
+        assert m[0] == [0, 1, math.sqrt(2)]
+        assert m[1] == [1, 0, math.sqrt(5)]
+        assert m[2] == [math.sqrt(2), math.sqrt(5), 0]
