156858323fa8c86cacbce28b8b1fd43dd3f73aca
diorge <diorge.bs@gmail.com>
2016-06-08 22:11:07 -0300

    Initial implementation of hierarchical clustering

--- src/hcluster.py
+++ src/hcluster.py
@@ -13,15 +13,75 @@ def euclidian(a, b):
 
 
 def distance_matrix(data, dist):
-    """ Distance matrix (all-to-all) """
-    m = []
-    for elem1 in data:
-        m.append([])
-        for elem2 in data:
-            m[-1].append(dist(elem1, elem2))
-    return m
+    """ Distance matrix (all-to-all), dictionary form """
+    return {(elem1, elem2): dist(elem1, elem2)
+            for elem1 in data
+            for elem2 in data
+            if elem1 != elem2}
+
+
+def min_index(matrix):
+    """
+    Gives the (i, j) index of the minimum value in the matrix
+    Ignores the main diagonal (expected values of zero)
+    """
+    m = float('inf')
+    v = None
+    for e1, e2 in matrix:
+        if e1 != e2 and matrix[(e1, e2)] < m:
+            m = matrix[(e1, e2)]
+            v = (e1, e2)
+    return v
+
+
+class HashableSet(set):
+
+    def __init__(self, *args):
+        set.__init__(self, args)
+
+    def __hash__(self):
+        h = 17
+        for x in self:
+            h = h ^ hash(x)
+        return h
+
+
+class Hierarchy(object):
+    
+    def __init__(self, data, matrix):
+        self._data = data[:]
+        self._matrix = matrix
+
+    def pair(self, elem1, elem2):
+        n = HashableSet(elem1, elem2)
+        for elem in self._data:
+            if elem not in n:
+                avg = (self._matrix[(elem, elem1)] + self._matrix[(elem, elem2)]) / 2
+                self._matrix[(elem, n)] = avg
+                self._matrix[(n, elem)] = avg
+                del self._matrix[(elem, elem1)]
+                del self._matrix[(elem, elem2)]
+                del self._matrix[(elem1, elem)]
+                del self._matrix[(elem2, elem)]
+        del self._matrix[(elem1, elem2)]
+        del self._matrix[(elem2, elem1)]
+        del self._data[self._data.index(elem1)]
+        del self._data[self._data.index(elem2)]
+        self._data.append(n)
+
+    def cluster(self):
+        while len(self._data) > 1:
+            e1, e2 = min_index(self._matrix)
+            self.pair(e1, e2)
+        
+
+    def setrepr(self):
+        return self._data[0]
 
 
 def cluster(data, dist=None):
     dist = dist or euclidian
     m = distance_matrix(data, dist)
+    h = Hierarchy(data, m)
+    h.cluster()
+    return h
--- src/test/tests.py
+++ src/test/tests.py
@@ -4,6 +4,7 @@ Unit tests for the hcluster module
 
 
 import hcluster
+from hcluster import HashableSet as hset
 import math
 from unittest import TestCase
 
@@ -19,7 +20,7 @@ class TestHCluster(TestCase):
         #   |-------|         |
         #   |       |     |-------|
         # (0, 0) (1, 1) (5, 5) (5, 6)
-        expected = set(set((0, 0), (1, 1)), set((5, 5), (5, 6)))
+        expected = hset(hset((0, 0), (1, 1)), hset((5, 5), (5, 6)))
         assert expected == c.setrepr()
 
     def test_cust_func(self):
@@ -27,12 +28,12 @@ class TestHCluster(TestCase):
         data = [(5, 0), (0, 0), (1, 2)]
         yDist = lambda a, b: abs(a[1] - b[1])
         c = hcluster.cluster(data, yDist)
-        expected = set(set((5, 0), (0, 0)), (1, 2))
+        expected = hset(hset((5, 0), (0, 0)), (1, 2))
         assert expected == c.setrepr()
 
     def test_distance_matrix(self):
         data = [(5, 5), (5, 6), (4, 4)]
         m = hcluster.distance_matrix(data, hcluster.euclidian)
-        assert m[0] == [0, 1, math.sqrt(2)]
-        assert m[1] == [1, 0, math.sqrt(5)]
-        assert m[2] == [math.sqrt(2), math.sqrt(5), 0]
+        assert m[((5, 5), (5, 6))] == 1
+        assert m[((4, 4), (5, 6))] == math.sqrt(5)
+        assert m[((5, 5), (4, 4))] == math.sqrt(2)
